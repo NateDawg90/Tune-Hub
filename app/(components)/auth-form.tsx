5472fa69d680fefc673978366d44ff09bc331219
Nate <rnatejohnson@gmail.com>
2024-05-28 23:55:05 -0500

    fix: style auth form

---
+++ app/(components)/auth-form.tsx
@@ -0,0 +1,90 @@
+'use client';
+import React, { useState } from 'react';
+
+const defaultFormData = {
+  email: '',
+  password: '',
+  firstName: '',
+};
+
+const AuthForm: React.FC = () => {
+  const [formData, setFormData] = useState(defaultFormData);
+
+  const handleFormDataChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // TODO: Add create user
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full bg-white p-8 border border-gray-300 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center mb-8">
+          Sign Up
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={formData.email}
+              onChange={handleFormDataChange}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={handleFormDataChange}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="firstName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              First Name:
+            </label>
+            <input
+              id="firstName"
+              value={formData.firstName}
+              onChange={handleFormDataChange}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import AuthForm from '@/components/auth-form';
+import AuthForm from '@/app/(components)/auth-form';
 import React from 'react';
 
 export const metadata = {
