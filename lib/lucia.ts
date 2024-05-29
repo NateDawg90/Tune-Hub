9154151fe3995c0a7c21a5008cb356c3627b5e6d
Nate <rnatejohnson@gmail.com>
2024-05-29 17:35:51 -0500

    fix: fixing bugs

--- lib/lucia.ts
+++ lib/lucia.ts
@@ -1,45 +1,32 @@
-// lib/lucia.ts
-import { Lucia } from 'lucia';
-import { MongoClient } from 'mongodb';
+// src/auth.ts
+import { Lucia, Session } from 'lucia';
 import { MongodbAdapter } from '@lucia-auth/adapter-mongodb';
-import { cookies } from 'next/headers';
 
-interface UserDoc {
-  _id: string;
-  // add other fields as needed
-}
+// connect to mongoose here
+import mongoose from 'mongoose';
+import connectToDb from './mongoose';
+import User from '@/app/(models)/User';
 
-interface SessionDoc {
-  _id: string;
-  user_id: string;
-  expires_at: Date;
-  // add other fields as needed
-}
+await connectToDb();
 
-const client = new MongoClient(process.env.MONGODB_URI!);
-await client.connect();
-const db = client.db('your-database-name'); // Specify your database name
-const User = db.collection<UserDoc>('users');
-const Session = db.collection<SessionDoc>('sessions');
-
+// your adapter
 const adapter = new MongodbAdapter(Session, User);
 
-const lucia = new Lucia(adapter, {
+export const lucia = new Lucia(adapter, {
   sessionCookie: {
+    // this sets cookies with super long expiration
+    // since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
     expires: false,
     attributes: {
-      sameSite: 'strict',
+      // set to `true` when using HTTPS
       secure: process.env.NODE_ENV === 'production',
     },
   },
 });
 
-export async function createAuthSession(userId: string) {
-  const session = await lucia.createSession(userId, {});
-  const sessionCookie = lucia.createSessionCookie(session.id);
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes
-  );
+// IMPORTANT!
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+  }
 }
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,20 +1,44 @@
-import mongoose, { connect } from 'mongoose';
+import mongoose from 'mongoose';
+declare global {
+  var mongoose: any;
+}
 
-// mongoose.connect(process.env.MONGODB_URI!);
-// mongoose.Promise = global.Promise;
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 async function connectToDb() {
-  // 4. Connect to MongoDB
-  try {
-    if (!process.env.MONGODB_URI) {
-      throw new Error('MONGODB_URI is not defined');
-    }
-    console.log('Connecting to MongoDB...');
-    await connect(process.env.MONGODB_URI!);
-    mongoose.Promise = global.Promise;
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
+  if (cached.conn) {
+    return cached.conn;
   }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
 
 export default connectToDb;
