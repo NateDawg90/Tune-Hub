720b4f4c8ea218bf141973396e1607f8db92b637
Nate <rnatejohnson@gmail.com>
2024-06-02 13:15:11 -0500

    fix: login and header styles

--- app/(components)/footer.tsx
+++ app/(components)/footer.tsx
@@ -4,18 +4,12 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col items-center justify-between px-4">
         <div className="flex space-x-4">
           {/* Affiliate links */}
-          <a
-            href="#"
-            className="text-jet-500 hover:text-davys-gray focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-davys-gray"
-          >
+          <button className="text-jet-500 hover:text-davys-gray focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-davys-gray">
             Affiliate Link 1
-          </a>
-          <a
-            href="#"
-            className="text-jet-500 hover:text-davys-gray focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-davys-gray"
-          >
+          </button>
+          <button className="text-jet-500 hover:text-davys-gray focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-davys-gray">
             Affiliate Link 2
-          </a>
+          </button>
         </div>
         <div className="text-jet-500 mt-4">
           © 2024 Nate Johnson. All rights reserved.
