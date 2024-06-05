e97f096ce5b1cc2b6f5230a4588c087e6b993c44
Nate <rnatejohnson@gmail.com>
2024-06-05 09:41:32 -0500

    fix: album fetching fix

--- app/api/albums/route.ts
+++ app/api/albums/route.ts
@@ -1,20 +1,11 @@
 // app/api/albums/route.ts
 import { Album } from '@/app/(models)';
-import connectToDb from '@/db/mongoose';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   try {
-    const params = req.nextUrl.searchParams;
-    const artistId = params.get('artistId');
-    if (!artistId) {
-      const albums = await Album.find().populate('artist').exec();
-      return NextResponse.json(albums, { status: 200 });
-    }
+    const albums = await Album.find().populate('artist').exec();
 
-    const albums = await Album.find({ artist: artistId })
-      .populate('artist')
-      .exec();
     return NextResponse.json(albums, { status: 200 });
   } catch (err) {
     console.error('Error fetching albums:', err);
