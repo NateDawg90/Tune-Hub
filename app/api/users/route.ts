98bfbe45da51263d013908e37b071e324a21f37f
Nate <rnatejohnson@gmail.com>
2024-06-08 17:15:01 -0500

    fix: connectot db before follow req

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,8 +3,8 @@ import connectToDb from '@/db/mongoose';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-  await connectToDb();
   try {
+    await connectToDb();
     const users = await User.find();
 
     return NextResponse.json(users, { status: 200 });
@@ -19,6 +19,7 @@ export async function GET(req: Request) {
 
 export async function POST(req: Request) {
   try {
+    await connectToDb();
     const body = await req.json();
     const userData = body.formData;
     await User.create(userData);
