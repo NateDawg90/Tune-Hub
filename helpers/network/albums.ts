c7ab84435507a215a70d780229c6d78abad62c6b
Nate <rnatejohnson@gmail.com>
2024-06-13 12:44:23 -0500

    perf: move fetch album data to server action file

--- helpers/network/albums.ts
+++ helpers/network/albums.ts
@@ -1,18 +1,6 @@
 import { Album } from '@/app/(models)/Album';
 import axios from 'axios';
 
-export const fetchAlbum = async (
-  id: string
-): Promise<Album | null> => {
-  try {
-    const response = await axios.get(`/api/albums/${id}`);
-    return response.data;
-  } catch (err) {
-    console.error('Error fetching album:', err);
-    return null;
-  }
-};
-
 export const fetchArtistAlbums = async (
   artistId: string
 ): Promise<Album[]> => {
